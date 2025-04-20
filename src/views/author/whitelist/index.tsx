7e728aee4c9b6f0f6dbaf7d5e76c87c8288b9d20
xqr66 <2943460598@qq.com>
2025-04-20 20:41:49 +0800

    修复白名单删除问题

--- src/views/author/whitelist/index.tsx
+++ src/views/author/whitelist/index.tsx
@@ -4,10 +4,10 @@ import Highlighter from 'react-highlight-words';
 import { connect } from "react-redux";
 import { DeleteOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
 import { Avatar, Button, Drawer, Form, Input, InputRef, message, Modal, Space, Table } from "antd";
-import type { ColumnsType,ColumnType } from "antd/es/table";
+import type { ColumnsType, ColumnType } from "antd/es/table";
 import { FilterConfirmProps } from "antd/es/table/interface";
 
-import { getAuthorWhiteListApi, resetAuthorWhiteApi, updateAuthorWhiteApi } from "@/api/modules/author";
+import { getAuthorWhiteListApi, resetAuthorWhiteApi, updateAuthorWhiteApi, delAuthorWhiteApi } from "@/api/modules/author";
 import { ContentInterWrap, ContentWrap } from "@/components/common-wrap";
 import { MapItem } from "@/typings/common";
 import AuthorSelect from "@/views/column/setting/components/authorselect";
@@ -22,7 +22,7 @@ interface DataType {
 	profile: string;
 }
 
-interface IProps {}
+interface IProps { }
 
 export interface IFormType {
 	authorId: number;
@@ -48,8 +48,8 @@ const AuthorWhiteList: FC<IProps> = props => {
 	const { authorName, authorId } = form;
 
 	const [searchText, setSearchText] = useState('');
-  const [searchedColumn, setSearchedColumn] = useState('');
-  const searchInput = useRef<InputRef>(null);
+	const [searchedColumn, setSearchedColumn] = useState('');
+	const searchInput = useRef<InputRef>(null);
 
 	const onSure = useCallback(() => {
 		setQuery(prev => prev + 1);
@@ -61,7 +61,7 @@ const AuthorWhiteList: FC<IProps> = props => {
 		console.log("handleChange item setForm", item, form);
 		formRef.setFieldsValue({ ...item });
 	};
-	
+
 	// 抽屉关闭
 	const handleClose = () => {
 		setIsDrawerOpen(false);
@@ -82,19 +82,19 @@ const AuthorWhiteList: FC<IProps> = props => {
 	type DataIndex = keyof DataType;
 
 	const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex,
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
+		selectedKeys: string[],
+		confirm: (param?: FilterConfirmProps) => void,
+		dataIndex: DataIndex,
+	) => {
+		confirm();
+		setSearchText(selectedKeys[0]);
+		setSearchedColumn(dataIndex);
+	};
 
 	const handleReset = (clearFilters: () => void) => {
-    clearFilters();
-    setSearchText('');
-  };
+		clearFilters();
+		setSearchText('');
+	};
 
 	// 删除
 	const handleDel = (userId: number) => {
@@ -106,7 +106,7 @@ const AuthorWhiteList: FC<IProps> = props => {
 			onOk: async () => {
 				const { status } = await delAuthorWhiteApi(userId);
 				const { code, msg } = status || {};
-	
+
 				if (code === 0) {
 					message.success("删除成功");
 					onSure();
@@ -129,7 +129,7 @@ const AuthorWhiteList: FC<IProps> = props => {
 		} else {
 			message.error(msg);
 		}
-		
+
 	};
 
 	// 数据请求
@@ -149,81 +149,81 @@ const AuthorWhiteList: FC<IProps> = props => {
 	}, [query]);
 
 	const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-          style={{ marginBottom: 8, display: 'block' }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            查询
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            重置
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText((selectedKeys as string[])[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            过滤
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            关闭
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ''}
-        />
-      ) : (
-        text
-      ),
-  });
+		filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+			<div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+				<Input
+					ref={searchInput}
+					placeholder={`Search ${dataIndex}`}
+					value={selectedKeys[0]}
+					onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+					onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+					style={{ marginBottom: 8, display: 'block' }}
+				/>
+				<Space>
+					<Button
+						type="primary"
+						onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+						icon={<SearchOutlined />}
+						size="small"
+						style={{ width: 90 }}
+					>
+						查询
+					</Button>
+					<Button
+						onClick={() => clearFilters && handleReset(clearFilters)}
+						size="small"
+						style={{ width: 90 }}
+					>
+						重置
+					</Button>
+					<Button
+						type="link"
+						size="small"
+						onClick={() => {
+							confirm({ closeDropdown: false });
+							setSearchText((selectedKeys as string[])[0]);
+							setSearchedColumn(dataIndex);
+						}}
+					>
+						过滤
+					</Button>
+					<Button
+						type="link"
+						size="small"
+						onClick={() => {
+							close();
+						}}
+					>
+						关闭
+					</Button>
+				</Space>
+			</div>
+		),
+		filterIcon: (filtered: boolean) => (
+			<SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+		),
+		onFilter: (value, record) =>
+			record[dataIndex]
+				.toString()
+				.toLowerCase()
+				.includes((value as string).toLowerCase()),
+		onFilterDropdownOpenChange: (visible) => {
+			if (visible) {
+				setTimeout(() => searchInput.current?.select(), 100);
+			}
+		},
+		render: (text) =>
+			searchedColumn === dataIndex ? (
+				<Highlighter
+					highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+					searchWords={[searchText]}
+					autoEscape
+					textToHighlight={text ? text.toString() : ''}
+				/>
+			) : (
+				text
+			),
+	});
 
 	// 表头设置
 	const columns: ColumnsType<DataType> = [
@@ -265,11 +265,11 @@ const AuthorWhiteList: FC<IProps> = props => {
 
 	// 编辑表单
 	const reviseDrawerContent = (
-		<Form 
-			name="basic" 
-			form={formRef} 
-			labelCol={{ span: 4 }} 
-			wrapperCol={{ span: 16 }} 
+		<Form
+			name="basic"
+			form={formRef}
+			labelCol={{ span: 4 }}
+			wrapperCol={{ span: 16 }}
 			autoComplete="off">
 			<Form.Item label="作者" name="author" rules={[{ required: true, message: "请选择作者!" }]}>
 				<AuthorSelect authorName={authorName} handleChange={handleChange} />
@@ -284,41 +284,41 @@ const AuthorWhiteList: FC<IProps> = props => {
 				<div className="author-whitelist-search">
 					<ContentInterWrap className="author-whitelist-search__wrap">
 						<div className="author-whitelist-search__search">
-							
+
 						</div>
 						<div className="author-whitelist-search__search-btn">
 							<Button
-							type="primary"
-							icon={<PlusOutlined />}
-							style={{ marginRight: "20px" }}
-							onClick={handleAdd}
+								type="primary"
+								icon={<PlusOutlined />}
+								style={{ marginRight: "20px" }}
+								onClick={handleAdd}
 							>
 								添加作者
 							</Button>
 						</div>
 					</ContentInterWrap>
 				</div>
-				
+
 				{/* 表格 */}
 				<ContentInterWrap>
 					<Table columns={columns} dataSource={tableData} />
 				</ContentInterWrap>
 			</ContentWrap>
 			{/* 抽屉 */}
-			<Drawer 
-				title="添加" 
-				open={isDrawerOpen} 
+			<Drawer
+				title="添加"
+				open={isDrawerOpen}
 				onClose={handleClose}
 				width={500}
 				extra={
-          <Space>
-            <Button onClick={handleClose}>取消</Button>
-            <Button type="primary" onClick={handleSubmit}>
-              确定
-            </Button>
-          </Space>
-        }
-				>
+					<Space>
+						<Button onClick={handleClose}>取消</Button>
+						<Button type="primary" onClick={handleSubmit}>
+							确定
+						</Button>
+					</Space>
+				}
+			>
 				{reviseDrawerContent}
 			</Drawer>
 		</div>
